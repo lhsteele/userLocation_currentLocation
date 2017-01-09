3ed7fe8bc98c68fdb41009319bd49912ffe8206e
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-01-09 11:03:03 -0800

    Initial Commit

---
+++ userLocation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  userLocation
+//
+//  Created by Lisa Steele on 1/9/17.
+//  Copyright © 2017 Lisa Steele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
