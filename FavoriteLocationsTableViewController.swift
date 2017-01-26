dd4576e87e9ff00d2767b94de5b023d3322eefd3
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-01-26 12:27:54 -0800

    Recreating deleted file

---
+++ FavoriteLocationsTableViewController.swift
@@ -0,0 +1,145 @@
+//
+//  favoriteLocationsTableViewController.swift
+//  userLocation
+//
+//  Created by Lisa H Steele on 1/10/17.
+//  Copyright © 2017 Lisa Steele. All rights reserved.
+//
+
+import UIKit
+
+let favoriteLocationKey = "NewFavoriteLocation"
+
+class FavoriteLocationsTableViewController: UITableViewController {
+    
+    
+    //var favorite = ""
+    
+    var listOfFavorites: [SavedFavorites] = []
+    var components = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let defaults = UserDefaults.standard
+        let favoriteLocations =  defaults.string(forKey: favoriteLocationKey)
+        
+        /*
+         
+         
+         if let newCell = favoriteLocations?.components(separatedBy: ";") {
+         let components = newCell
+         }
+         */
+        
+        let components = favoriteLocations?.components(separatedBy: ";")
+        
+        print (components)
+        
+        var output = [(lat: "", long: "", location: "")]
+        
+        
+        for component in components! {
+            if !component.isEmpty {
+                
+                let subComponents = component.components(separatedBy: ";")
+                let tuple = (lat: subComponents[0], long: subComponents[1], location: subComponents[2])
+                
+                print (tuple)
+                //output.append(tuple)
+            }
+        }
+        
+        //print (output)
+        
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    /*
+     func addFavorite() {
+     let favorite = SavedFavorites(location: "", coordinate: "")
+     self.listOfFavorites.append(favorite)
+     let newIndexPath = IndexPath(row: self.listOfFavorites.count - 1, section: 0)
+     self.tableView.insertRows(at: [newIndexPath], with: .automatic)
+     }
+     */
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return self.listOfFavorites.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let favorite = self.listOfFavorites[indexPath.row]
+        
+        if let coordinate = favorite.coordinate {
+            cell.textLabel?.text = coordinate
+        } else {
+            cell.textLabel?.text = "No Coordinates Saved"
+        }
+        
+        return cell
+    }
+    
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+}
