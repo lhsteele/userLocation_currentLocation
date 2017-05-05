d4258ed7e1a001de72d1af17691ccd02f5c3437d
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-05-05 14:35:32 -0700

    Change password now working.

--- userLocation/NewPasswordViewController.swift
+++ userLocation/NewPasswordViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
-class NewPasswordViewController: UIViewController {
+class NewPasswordViewController: UIViewController, UITextFieldDelegate{
+    
+    @IBOutlet var newPasswordTextField: UITextField!
+    @IBOutlet var submitButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +26,49 @@ class NewPasswordViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func submitToChangePassword(_ sender: Any) {
+        changePassword()
     }
-    */
+    
+    func changePassword() {
+        let user = FIRAuth.auth()?.currentUser
+        user?.updatePassword(newPasswordTextField.text!, completion: { (error) in
+            if let error = error {
+                self.displayAlertMessage(messageToDisplay: "There has been an error updating your password. Please try again.")
+            } else {
+                self.displaySuccessAlertMessage(messageToDisplay: "Your password has been successfully changed. Please login with your new password.")
+            }
+        })
+    }
+    
+    func displayAlertMessage(messageToDisplay: String) {
+        let alertController = UIAlertController(title: "Error", message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+        }
+        
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion:nil)
+    }
+    
+    func displaySuccessAlertMessage(messageToDisplay: String) {
+        let alertController = UIAlertController(title: "Success", message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            self.performSegue(withIdentifier: "PasswordChangedLoginSegue", sender: self)
+        }
+        
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion:nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        newPasswordTextField.resignFirstResponder()
+        return true
+    }
+
 
 }
