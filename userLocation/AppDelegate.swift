a10469d438672bf58044204ef6317b2e3f440b35
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-05-15 21:19:33 -0700

    Trying to implement user notifications. Have been able to print the device token to console.

--- userLocation/AppDelegate.swift
+++ userLocation/AppDelegate.swift
@@ -23,21 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FIRApp.configure()
         
         if #available(iOS 10, *) {
-            let center = UNUserNotificationCenter.current()
-            center.delegate = self as! UNUserNotificationCenterDelegate
-            center.requestAuthorization(options: [.badge, .sound, .alert], completionHandler: { (grant, error) in
-                if error == nil {
-                    if grant {
-                        application.registerForRemoteNotifications()
-                    } else {
-                        //User didn't grant permission
-                    }
-                    } else {
-                        print ("error: \(error)")
-                }
+            UNUserNotificationCenter.current().requestAuthorization(options: [.badge, .alert,.sound], completionHandler: { (granted, error) in
+                application.registerForRemoteNotifications()
             })
         } else {
-            //fallback on earlier versions
             let notificationSettings = UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
             application.registerUserNotificationSettings(notificationSettings)
         }
@@ -45,7 +34,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-  
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print (deviceToken)
+        let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+        print("APNs device token: \(deviceTokenString)")
+        //persist device token in backend
+        //user UserDefaults to persist token locally and only update backend when token has changed, to avoid unnecessary requests.
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print ("APNs registration failed: \(error)")
+    }
+    
+    //This callback will only be invoked when user has either clicked or swiped to interact with push notification from the lock screen/Notification Center, or if app was open when notification was received by device.
+    //Need to write logic that gets executed when notification is interacted with.
+    //If app is open when a notification is received, user will not see it at all. Need to find another way to notify user.
+    func application(_ application: UIApplication, didReceiveRemoteNotification data: [AnyHashable : Any]) {
+        print ("Push notification received: \(data)")
+    }
     
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
