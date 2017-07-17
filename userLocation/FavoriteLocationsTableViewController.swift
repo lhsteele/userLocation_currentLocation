e409cef3c280c187a1d7000ab91650f3009f8e38
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-07-16 20:30:58 -0700

    Logic to check for existing live journey rewritten so that it checks for the boolean value, not just for an existing snapshot.

--- userLocation/FavoriteLocationsTableViewController.swift
+++ userLocation/FavoriteLocationsTableViewController.swift
@@ -389,14 +389,24 @@ class FavoriteLocationsTableViewController: UITableViewController, CLLocationMan
     }
     
     func checkForExistingLiveJourney() {
-        let databaseRef = FIRDatabase.database().reference(fromURL: "https://userlocation-aba20.firebaseio.com/").child("StartedJourneys").queryOrderedByKey()
         if let userID = FIRAuth.auth()?.currentUser?.uid {
-            _ = databaseRef.queryEqual(toValue: userID).observeSingleEvent(of: .value, with: { (snapshot) in
-                print (snapshot)
-                if snapshot.exists() {
-                      self.displayErrorAlertMessage(messageToDisplay: "You are currently on a journey and have already shared it with another user.")
-                } else {
-                    self.performSegue(withIdentifier: "StartJourneySegue", sender: Any.self)
+        let databaseRef = FIRDatabase.database().reference(fromURL: "https://userlocation-aba20.firebaseio.com/").child("StartedJourneys").child(userID)
+            _ = databaseRef.observeSingleEvent(of: .value, with: { (snapshot) in
+                let children = snapshot.children
+                for item in children {
+                    if let pair = item as? FIRDataSnapshot {
+                        print (pair)
+                        if let boolean = pair.value as? Bool {
+                            let boolKey = pair.key
+                            if boolKey == "JourneyEnded" {
+                                if boolean != true {
+                                    self.displayErrorAlertMessage(messageToDisplay: "You are currently on a journey and have already shared it with another user.")
+                                } else {
+                                    self.performSegue(withIdentifier: "StartJourneySegue", sender: Any.self)
+                                }
+                            }
+                        }
+                    }
                 }
             })
         }
