30ee41c3a6edab7f79ac08128061fd327aa593e4
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-08-08 20:08:48 -0700

    Device orientation restricted to portrait

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,5 @@
-// This registration token comes from the client FCM SDKs.
 var functions = require('firebase-functions');
 var admin = require('firebase-admin')
-//var registrationToken = "f2og_pbqadc:APA91bH6MJynSoPqqDzxGDKkfUJf6zwGatniXTU5HrRpEYlpSC2Y4TNBZnAiHnrlowFgx1E8MFavixW6Hnp_nQioY6tO4dc6LyAAEanE0Sxh-7T_91yb596Mpyh9C_urD2BZ-bu8Hx_W";
 var userDeviceToken = "";
 var sharedUserID = "";
 
@@ -15,7 +13,6 @@ var payloadStart = {
   notification: {
     title: "See You Soon!",
     body: "Someone has shared a journey with you."
-    //sound: 'default',
   },
 }
 
@@ -27,27 +24,12 @@ var payloadEnd = {
   notification: {
     title: "See You Soon!",
     body: "A shared journey has ended."
-    //sound: 'default',
   },
 }
 
 exports.newEntry = functions.database.ref('/StartedJourneys/{fireUserID}')
   .onWrite(event => {
     const original = event.data.val()
-    //console.log(original)
-    //console.log(original.SharedWithUserID)
-    //console.log(original.JourneyEnded)
-    //console.log(event.data.changed())
-    //console.log(event.data.exists())
-    //console.log(event.data.previous)
-    //console.log(event.params)
-    /*
-    var payload = payloadStart
-
-    if (original.JourneyEnded) {
-    	payload = payloadEnd
-    } 
-    */
     
     if (original.JourneyEnded != true) {
     	var payload = payloadStart;
@@ -56,19 +38,14 @@ exports.newEntry = functions.database.ref('/StartedJourneys/{fireUserID}')
     }
 
     sharedUserID = original.SharedWithUserID;
-    //console.log(sharedUserID)
     var db = admin.database();
     var ref = db.ref('/UserTokens');
     
     return ref.orderByKey().equalTo(sharedUserID).on("child_added", function(snapshot) {
       const deviceToken = snapshot.val();
-      //userDeviceToken = deviceToken
-      //console.log(userDeviceToken)
 
       admin.messaging().sendToDevice(deviceToken, payload, options)
 	  	.then(function(response) {
-	  	// See the MessagingDevicesResponse reference documentation for
-	  	// the contents of response.
 	    	console.log("Successfully sent message:", response);
 	  	})
 	  	.catch(function(error) {
