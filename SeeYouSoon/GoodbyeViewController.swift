91e0f83fc9a4a8fa345c3b48461dc994e054689f
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-08-17 15:45:13 -0700

    Back button on the Goodbye VC hidden. Onboarding also changed so user only sees it once.

--- SeeYouSoon/GoodbyeViewController.swift
+++ SeeYouSoon/GoodbyeViewController.swift
@@ -16,6 +16,8 @@ class GoodbyeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
         view.backgroundColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
         label.textColor = UIColor(red: 0.93, green: 0.95, blue: 0.95, alpha: 1.0)
     }
