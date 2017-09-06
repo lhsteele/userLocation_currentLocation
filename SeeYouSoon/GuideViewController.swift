ca343c710f9b8d5ff1f564cca8c96ba323ffeecc
Lisa Steele <lisasteele@Lisas-Mac-mini.local>
2017-09-06 10:49:38 -0700

    Stack view and layout issues resolved. Guide VC complete.

--- SeeYouSoon/GuideViewController.swift
+++ SeeYouSoon/GuideViewController.swift
@@ -10,17 +10,38 @@ import UIKit
 
 class GuideViewController: UIViewController {
 
-    @IBOutlet var text: UITextView!
+    @IBOutlet var step1Label: UILabel!
+    @IBOutlet var step2Label: UILabel!
+    @IBOutlet var step3Label: UILabel!
+    @IBOutlet var step4Label: UILabel!
+    
+    @IBOutlet var step1Text: UITextView!
+    @IBOutlet var step2Text: UITextView!
+    @IBOutlet var step3aText: UITextView!
+    @IBOutlet var step3bText: UITextView!
+    @IBOutlet var step3cText: UITextView!
+    @IBOutlet var step4Text: UITextView!
+    
     @IBOutlet var closebutton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
         closebutton.setTitleColor(UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0), for: UIControlState.normal)
         closebutton.layer.borderColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0).cgColor
         closebutton.layer.borderWidth = 1
         closebutton.layer.cornerRadius = 10
+        
+        step1Label.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step2Label.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step3Label.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step4Label.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step1Text.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step2Text.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step3aText.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step3bText.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step3cText.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
+        step4Text.textColor = UIColor(red: 0.23, green: 0.44, blue: 0.51, alpha: 1.0)
         
         self.navigationController?.setNavigationBarHidden(true, animated: true)
     }
